b3a28bad8c02178bf6661dd572c2f50ebfe2457a
MattWagner28 <matthewwagner@outlook.com>
2023-05-04 20:31:12 -0400

    got server going

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+// const User = require('../models');
 
 // Login route
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     // if (req.session.loggedIn) {
     //   res.redirect('/');
     //   return;
     // }
     res.render('login');
   });
+
+
+  module.exports = router;
