2525db39bce50e488f4eb61b8b26ce1de1f71f09
MattWagner28 <matthewwagner@outlook.com>
2023-05-09 19:30:55 -0400

    commented out browser alerts

--- public/js/login.js
+++ public/js/login.js
@@ -38,10 +38,10 @@ const signupFormHandler = async (event) => {
     });
     console.log(response);
     if (response.ok) {
-      alert('you did it');
+      // alert('you did it');
       document.location.replace(`/dashboard?email=${email}`);
     } else {
-      alert('Failed to sign up.');
+      // alert('Failed to sign up.');
     }
     console.log(response)
   }
